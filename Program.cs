8b8655e2477a2496ebddd114d412920b215845ba
kushlover-air-g <59054712+kushlover-air-g@users.noreply.github.com>
2019-12-19 20:07:00 +0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите x:");
+            int x = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите n:");
+            int n = Convert.ToInt32(Console.ReadLine());
+            double symm = 1 / x;
+            for(int i = 1; i<=n; i++)
+            {
+                symm += 1 / (x + i);
+            }
+            Console.WriteLine("Сумма равна = " + symm);
+        }
+    }
+}
